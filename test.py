32f8e43842a74d99e1001b005d68afd30947d58d
Matt Thompson <mattt@defunct.ca>
2018-03-13 13:37:34 -0400

    Add versions to YAML and tidy up cli

--- test.py
+++ test.py
@@ -1,46 +1,50 @@
-import yaml
 import sys
+import yaml
+
+import click
 
 
-def main():
-    stream = file('products.yml', 'r')
-
-    data = yaml.load(stream)
-
-    if len(sys.argv) != 2:
-        sys.exit(1)
-
-    if sys.argv[1] == "products":
-        print(products(data))
-    elif sys.argv[1] == "components":
-        print(components(data))
-    else:
-        print(component_in_products(data, sys.argv[1]))
+stream = file('products.yml', 'r')
+data = yaml.load(stream)
 
 
-def products(data):
-    products = []
-    for product in data:
-        products.append(product)
-    return products
-
-
-def components(data):
+def get_components(product=None):
     c = []
-    for product in data:
-        for component in data[product]:
-            if component not in c:
-                c.append(component)
+    for p in data['products']:
+        if (product and product == p['name']) or product is None:
+            for component in p['components']:
+                if component['name'] not in c:
+                    c.append(component['name'])
     return c
 
 
-def component_in_products(data, component):
-    products = []
-    for product in data:
-        if component in data[product]:
-            products.append(product)
-    return products
+def get_products(component=None):
+    p = []
+    for product in data['products']:
+        if (component and component in get_components(product['name'])) or component is None:
+            p.append(product['name'])
+    return p
 
 
-if __name__ == "__main__":
-    main()
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.option('--component')
+def products(component):
+    p = get_products(component)
+    print(p)
+
+
+@click.command()
+def components():
+    c = get_components()
+    print(c)
+
+
+cli.add_command(products)
+cli.add_command(components)
+
+cli()
